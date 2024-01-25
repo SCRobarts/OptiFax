a36bc9a1e11b9a7485d1f71581cc9f5eb81e61f6
SCRobarts <sebrobarts@gmail.com>
2024-01-25 17:51:46 +0000

    Add astroguideSim

---
+++ toolbox/astroguideSim.m
@@ -0,0 +1,83 @@
+%% astroguideSim.m
+% WIP script to run supercontinuum generation for the SALT astrocomb.
+%
+%	Sebastian C. Robarts 2024 - sebrobarts@gmail.com
+
+close all
+clear
+
+%% Initialise Laser / Input Pulse
+load("Taccor800.mat");
+laser.AveragePower = 0.3; 
+
+%% Initialise Simulation Window
+lambda_ref = laser.Wavelength;
+npts = 2^18;
+tAxis = 12e-12;
+tOff = 0;
+simWin = SimWindow(lambda_ref,npts,tAxis,tOff);
+simWin.Limits = [210 6500];
+
+laser.simulate(simWin);
+laser.Pulse.plot;
+
+% nr = laser.Pulse.Medium.Bulk.RefractiveIndex; 
+% Esq2I = nr.*c.*eps0 / 2;
+% IT = Esq2I .* ((laser.Pulse.TemporalField)).^2;
+% 
+% % AT = sqrt(laser.Pulse.TemporalIntensity .* laser.Area);
+% AT = sqrt(IT .*  laser.Area);
+% laser.Pulse.TemporalIntensity = (AT.^2) ./ laser.Area;
+% 
+% laser.Pulse.plot;
+% return
+
+%% Generate fibre supercontinuum
+load("FemtoWHITE_CARS.mat");
+[~] = gnlsefibsim(laser,simWin,fibre);
+
+%% Initialise Optical Cavity
+% chirp = 2000*1e-30;
+% load('pbCav.mat');
+load("ChirpedWaveguideLN.mat")
+% crystal.Length = 1e-3;
+cav = Cavity(crystal,0);
+% [Z, AT, AW, W, B] = gnlsefibsim(laser,simWin,fibre);
+
+
+sc = laser.Pulse.writeto;
+sc.plot;
+load("Taccor800.mat")
+laser.AveragePower = 1;
+
+% return
+%% Optical Simulation Setup
+delay = -250e-15;
+errorBounds = [1e-5,1e-3];	% Percentage error tolerance
+minStep = 0.05e-6;		% Minimum step size
+% errorBounds = [1e-2,1e-0];	% Percentage error tolerance
+% minStep = 0.25e-6;		% Minimum step size
+
+optSim = OpticalSim(laser,cav,simWin,errorBounds,minStep);
+optSim.RoundTrips = 1;
+optSim.ProgressPlots = 300;
+% optSim.Hardware = "CPU";
+
+optSim.setup;
+optSim.Pulse.copyfrom(sc);
+optSim.Pulse.applyGD(6*delay)
+optSim.PumpPulse.applyGD(7*delay);
+optSim.run;
+
+lIW = 10*log10(abs(optSim.Pulse.SpectralField).^2);	% log scale spectral intensity
+lIW = smooth(lIW,0.0005).';
+mlIW = max(max(lIW));							% max value, for scaling plot
+pIW = 10*log10(abs(optSim.PumpPulse.SpectralField).^2);	% log scale spectral intensity
+mpIW = max(max(pIW));							% max value, for scaling plot
+
+figure
+plot((simWin.Omegas./(2*pi))*1e-12,[lIW-mpIW;pIW-mpIW])
+xlim([230, 800])
+ylim([-35.0, 0])
+xlabel("Frequency/THz")
+ylabel("Relative Intensity/dB")
